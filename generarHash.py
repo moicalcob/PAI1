212d0cad5dafaa7add1f8593f7e0b5769ac559a3
Amine <amichachi@alum.us.es>
2021-03-21 18:16:02 +0100

    Integrada la funcionalidad generar Hash y buscar hash con la BBDD

--- generarHash.py
+++ generarHash.py
@@ -3,8 +3,12 @@ import os.path as path
 import os 
 from pathlib import Path
 from _sha256 import sha256
+from datetime import date
+from datetime import datetime 
+import sqlite3
 import sys,os
 import csv
+from django.template.defaultfilters import length
 try:
     import pandas as pd
 except:
@@ -14,6 +18,33 @@ from fnmatch import fnmatch
 path = "archivos"
 pathTemp = "/Users/amine/OneDrive - UNIVERSIDAD DE SEVILLA/INGLES FIRST"
 root = "/Users/moises/Downloads" # it may have many subfolders and files inside
+
+##BASES DE DATOS ##
+
+
+def create_table():
+    c.execute("CREATE TABLE IF NOT EXISTS ficheros(nombre TEXT, hash TEXT, fecha TEXT)")
+
+def insertData(nombre, hash):
+    fecha = datetime.now()
+    datos = []
+    datos.append(nombre)
+    datos.append(hash)
+    datos.append(fecha)
+    c.execute("INSERT INTO ficheros (nombre, hash, fecha) values (?, ?, ?)", datos)   
+    conn.commit()
+
+#FUNCION QUE BUSCA UN FICHERO Y SU HASH EN LA BBDD, Si ambos campos exiten OK sino pa tu casa
+def cogerHash(nombre,hash):
+    c.execute("SELECT * FROM ficheros WHERE nombre = ? AND hash = ?", (nombre,hash))
+    rows = c.fetchall()
+    if len(rows) == 0:
+       print("NO SE HA ENCONTRADO EL ARCHIVO SOLICITADO")
+    else:  
+       for r in rows: 
+           print(r)
+
+## Explora directorios y llama a generteHash Para generar los hashes
 def explorarDirectorios():
     lst = []
     pattern = "*.*"        # Note: Use this pattern to get all types of files and folders 
@@ -22,7 +53,8 @@ def explorarDirectorios():
             if fnmatch(name, pattern):
                 lst.append((os.path.join(path, name)))
     generateHash(lst)
-    
+
+## GENERA EL HASH DE CADA FICHERO Y LO GUARDA EN BBDD
 def generateHash(file_list):
     data = {}
     i = 0
@@ -32,11 +64,9 @@ def generateHash(file_list):
             with open(file, "rb") as f:
                 for bloque in iter(lambda: f.read(65536), b""):
                     sha256.update(bloque)
+            insertData(file, sha256.hexdigest())
             data[file] = sha256.hexdigest()
         print (data)
-        return data
-#        df = pd.DataFrame({"Fichero;Hash": datos})
-#        df.to_csv("hashes2.csv")
     except Exception as e: 
         print ("ERROR: %s" % (e))
         return ""
@@ -44,4 +74,8 @@ def generateHash(file_list):
         print("Error desconocido")    
     
 if __name__ == "__main__":
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
     explorarDirectorios()
+    cogerHash("Luis","IOASUEHQADGNQIUEH3258723943KNDIF")
+    conn.close()
