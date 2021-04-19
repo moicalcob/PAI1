511fc8bc0f9a37fced196beb95aad5f20d5cad66
Moisés Calzado <moisescalzado1227@gmail.com>
2021-04-19 16:21:58 +0200

    Completo el encriptado BLOWFISH

--- CAI3/src/Main.java
+++ CAI3/src/Main.java
@@ -1,8 +1,69 @@
 import encryption.AES_CBC_128;
+import encryption.BLOWFISH;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        AES_CBC_128.runAES_CBC_128();
+        System.out.println("1: Encriptar con BLOWFISH");
+        System.out.println("2: Encriptar con AES_CBC_128");
+        System.out.println("0: Exit");
+
+        while (true) {
+            char input = in.next().toCharArray()[0];
+            switch (input) {
+                case '1':
+                    System.out.println("Encriptando con BLOWFISH");
+                    String encryptedBLOWFISHImage = BLOWFISH.runBLOWFISH();
+                    writeEncryptedFile(encryptedBLOWFISHImage, "blowfish_image.txt");
+                    break;
+                case '2':
+                    System.out.println("Encriptando con AES_CBC_128");
+                    String encryptedAES_CBC_128Image = AES_CBC_128.runAES_CBC_128();
+                    writeEncryptedFile(encryptedAES_CBC_128Image, "AES_CBC_128_image.txt");
+                    break;
+                case '0':
+                    System.exit(0);
+                    break;
+                default:
+            }
+        }
+    }
+
+    private static void writeEncryptedFile(String encryptedContent, String fileName) {
+        try {
+            File file = new File("./" + fileName);
+            FileWriter myWriter = new FileWriter("./" + fileName);
+            myWriter.write(encryptedContent);
+            myWriter.close();
+            printFileSize("./" + fileName);
+            if (file.createNewFile()) {
+                System.out.println("Archivo encriptado guardado: " + file.getName());
+            } else {
+                System.out.println("Archivo actualizado correctamente.");
+            }
+        } catch (IOException e) {
+            System.out.println("Ha ocurrido un error.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void printFileSize(String fileName) {
+        Path path = Paths.get(fileName);
+        try {
+            long bytes = Files.size(path);
+            System.out.printf("Tamaño del archivo: %,d kilobytes%n", bytes / 1024);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- CAI3/src/encryption/BLOWFISH.java
+++ CAI3/src/encryption/BLOWFISH.java
@@ -3,7 +3,6 @@ package encryption;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,59 +11,52 @@ import java.nio.charset.StandardCharsets;
 import static org.apache.commons.codec.binary.Base64.decodeBase64;
 import static org.apache.commons.codec.binary.Base64.encodeBase64;
 
-public class AES_CBC_128 {
+public class BLOWFISH {
 
     private static final String clave = "ClaveST22-CAI3@1";
-    private static final String iv = "0123456789ABCDEF"; // vector de inicialización
 
-    public static String runAES_CBC_128() {
+    public static String runBLOWFISH() {
         try {
             File f = new File("./imagenes/shutterstock_392691841.jpeg");
             String encodedImage = encodeFileToBase64Binary(f);
 
             long startTime = System.nanoTime();
-            String encrypted_AES_CBC_128 = AES_CBC_128.encryptAES_CBC_128(clave, iv, encodedImage);
+            String encrypted_BLOWFISH = BLOWFISH.encryptBLOWFISH(clave, encodedImage);
             long endTime = System.nanoTime();
             long timeElapsed = endTime - startTime;
             System.out.println("Tiempo de encriptado: " + timeElapsed / 1000000 + "ms");
 
             startTime = System.nanoTime();
-            String decrypted_AES_CBC_128 = AES_CBC_128.decryptAES_CBC_128(clave, iv, encrypted_AES_CBC_128);
+            String decrypted_BLOWFISH = BLOWFISH.decryptBLOWFISH(clave, encrypted_BLOWFISH);
             endTime = System.nanoTime();
             timeElapsed = endTime - startTime;
             System.out.println("Tiempo de desencriptado: " + timeElapsed / 1000000 + "ms");
 
-            System.out.println("Imagen se conserva de manera íntegra: " + encodedImage.equals(decrypted_AES_CBC_128));
-            return encrypted_AES_CBC_128;
+            System.out.println("Imagen se conserva de manera íntegra: " + encodedImage.equals(decrypted_BLOWFISH));
+            return encrypted_BLOWFISH;
         } catch (Exception error) {
             System.out.println(error.getMessage());
             return "";
         }
     }
 
-    public static String encryptAES_CBC_128(String key, String iv, String cleartext) throws Exception {
-        // Definición del tipo de algoritmo a utilizar (AES, DES, RSA)
-        String alg = "AES";
-        // Definición del modo de cifrado a utilizar
-        String cI = "AES/CBC/PKCS5Padding";
+    public static String encryptBLOWFISH(String key, String cleartext) throws Exception {
+        String alg = "Blowfish";
+        String cI = "Blowfish";
         Cipher cipher = Cipher.getInstance(cI);
         SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), alg);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
-        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
         byte[] encrypted = cipher.doFinal(cleartext.getBytes());
         return new String(encodeBase64(encrypted));
     }
 
-    public static String decryptAES_CBC_128(String key, String iv, String encrypted) throws Exception {
-        // Definición del tipo de algoritmo a utilizar (AES, DES, RSA)
-        String alg = "AES";
-        // Definición del modo de cifrado a utilizar
-        String cI = "AES/CBC/PKCS5Padding";
+    public static String decryptBLOWFISH(String key, String encrypted) throws Exception {
+        String alg = "Blowfish";
+        String cI = "Blowfish";
         Cipher cipher = Cipher.getInstance(cI);
         SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), alg);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
         byte[] enc = decodeBase64(encrypted);
-        cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivParameterSpec);
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
         byte[] decrypted = cipher.doFinal(enc);
         return new String(decrypted);
     }
